51997a53fe120dc476f919d8cf6dced66059c3bc
daehan.lawplatform <daehan@lawplatform.kr>
2023-08-09 16:56:05 +0900

    sync:current

---
+++ src/pages/Conssul.tsx
@@ -0,0 +1,12 @@
+export default function Conssul() {
+	return (
+		<div>기능 소개  https://pixijs.com/
+			-상담가를 위한 상담 공간 서비스
+			- 홍보를 위한 전문가가 제작하는 메타버스 공간 제공
+			- 자료 관리를 위한 자료실
+			-
+
+		</div>
+	)
+
+}
