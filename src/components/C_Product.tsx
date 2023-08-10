1fdabb58e314088aa3a6b7aa56a73472c7cf317b
daehan.lawplatform <daehan@lawplatform.kr>
2023-08-10 09:31:04 +0900

    fix: componetnes error

--- src/components/C_Product.tsx
+++ src/components/C_Product.tsx
@@ -4,7 +4,7 @@ export default function C_Product() {
 		<div>
 			<div className="container px-5 py-24 mx-auto">
 				<div className="lg:w-4/5 mx-auto flex flex-wrap">
-					<img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://stackdiary.com/140x100.png" />
+					<Image alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://stackdiary.com/140x100.png" />
 					<div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0" >
 						<h2 className="text-sm title-font text-gray-500 tracking-widest" >ON SALE</h2>
 						<h1 className="text-gray-900 text-3xl title-font font-medium mb-1" >Blue Dress v2</h1>
