4507b73afe757425cd03bf00e811dc7afd6149aa
daehan.lawplatform <daehan@lawplatform.kr>
2023-08-14 16:01:12 +0900

    feat: try to design conssul link

--- src/pages/Conssul.tsx
+++ src/pages/Conssul.tsx
@@ -1,22 +1,34 @@
 import Des from "../components/Des";
-
+import Image from "next/image"
 export default function Conssul() {
 	return (
 
 
 		<div className="md:flex items-center">
 			<div className="w-full md:w-1/2 mb-10 md:mb-0">
-				<div className="w-ful bg-red-100">arst</div>
+				<div className="w-ful bg-red-100">
+
+
+					<div className="avatar">
+						<div className="w-24 rounded-full">
+							<img src="/temp/person.jpg" />
+						</div>
+					</div>
+
+					<div className="avatar fixed top-5">
+						<div className="w-24 rounded-full">
+							<img src="/temp/person.jpg" />
+						</div>
+					</div>
+
+				</div>
 			</div>
-			<div className="w-full md:w-1/2">
+			<div className="w-full md:w-1/2 ">
 				<Des Title={"컨썰"} Text={"컨썰턴트와 썰을 풀다"}></Des>
 
 
-				- 메타버스 공간을 활용한 중계 플랫폼 서비스
-				- 메타버스를 통한 경험과 지식을 나눌수 있는 가상의 공간 제공
-				- 자료 관리를 위한 자료실
-				- 유연한 스케줄링 제공
-				<button className="btn btn-neutral">방문하기 </button>
+				<p>메타버스 공간을 활용한 중계 플랫폼 서비스</p>
+				<button className="btn btn-neutral mt-5 mb-5">방문하기 </button>
 			</div>
 		</div>
 
--- src/pages/Container.tsx
+++ src/pages/Container.tsx
@@ -3,13 +3,13 @@ import C_feat from "../components/C_feat";
 export default function Container() {
 	return (
 		<div className="hero-content w-full md:w-1/2 max-w-6xl rounded bg-white shadow-xl p-10 lg:p-20 mx-auto  ">
-			<div className="md:flex items-center">
-				<div className="">
-					<C_feat title="소통" desc="고객과의 연결" img="temp" />
+			<div className="md:flex items-center overflow-hidden">
+				<div className="avatar">
+					<div className="w-24 rounded-full">
+						<img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+					</div>
 				</div>
-				<div className="w-full md:w-1/2">
-					<C_feat title="연결" desc="고객과의 연결" img="temp" />
-				</div>
+
 			</div>
 		</div>
 
