c12f4e45e4b40753b30b8e92806b0397610b932a
daehan.lawplatform <daehan@lawplatform.kr>
2023-08-24 10:55:02 +0900

    fix: change apply aniamtion part

--- app/main/work/page.tsx
+++ app/main/work/page.tsx
@@ -26,12 +26,12 @@ export default function Home() {
 				<T_1 title={"컨썰"} text={"컨썰턴트와 썰을 풀다"}></T_1>
 				<Conssul></Conssul>
 				<div className="mx-auto grid grid-cols-1 md:grid-cols-3 gap-1 sm:gap-4">
-					<C_feat_v img={"temp"} title={"메타버스 공간 상담 플랫폼"} text={"홍보에 필요한 메타버스 공간 제작"} />
-					<C_feat_v img={"temp"} title={"자료실 기능"} text={"자료 관리를 위한 자료실 제공"} />
-					<C_feat_v img={"temp"} title={"상담사를 위한 일정관리 "} text={"30분 단위로 유연한 스케줄 관리"} />
-					<C_feat_v img={"temp"} title={"통합 컨설팅 솔루션"} text={"입시, 법률, 세무.. 모든 상담 관리"} />
-					<C_feat_v img={"temp"} title={"수수료 무료"} text={"상담사를 위한 수수료 무료 서비스"} />
-					<C_feat_v img={"temp"} title={"다양한 공간 제공"} text={"36가지 youstory 기본 테마 제공"} />
+					<C_feat_v img={"planet"} title={"메타버스 공간 상담 플랫폼"} text={"홍보에 필요한 메타버스 공간 제작"} />
+					<C_feat_v img={"db"} title={"자료실 기능"} text={"자료 관리를 위한 자료실 제공"} />
+					<C_feat_v img={"calendar"} title={"상담사를 위한 일정관리 "} text={"30분 단위로 유연한 스케줄 관리"} />
+					<C_feat_v img={"group"} title={"통합 컨설팅 솔루션"} text={"입시, 법률, 세무.. 모든 상담 관리"} />
+					<C_feat_v img={"money"} title={"수수료 무료"} text={"상담사를 위한 수수료 무료 서비스"} />
+					<C_feat_v img={"small-shop"} title={"다양한 공간 제공"} text={"36가지 youstory 기본 테마 제공"} />
 				</div>
 				{/* 컨썰 */}
 			</div>
