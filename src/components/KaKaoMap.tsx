5423a453a7824753872b8bdaef60345b62538411
daehan.lawplatform <daehan@lawplatform.kr>
2023-08-24 16:23:24 +0900

    feat: add map marker

--- src/components/KaKaoMap.tsx
+++ src/components/KaKaoMap.tsx
@@ -11,9 +11,21 @@ export default function KaKaoMap() {
 				center={{ lat: 37.65286781200174, lng: 126.77620524924484 }}
 				style={{ width: "100%", height: "500px" }}
 			>
-				<MapMarker position={{ lat: 37.65286781200174, lng: 126.77620524924484 }}>
-
-				</MapMarker>
+				<MapMarker position={{ lat: 37.65286781200174, lng: 126.77620524924484 }}
+					image={{
+						src: "/icon/marker.png",
+						size: {
+							width: 64,
+							height: 69,
+						},
+						options: {
+							offset: {
+								x: 27,
+								y: 69,
+							},
+						},
+					}}
+				/>
 			</Map>
 		</div>
 	)
