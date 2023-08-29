0d0087b28e49b9fd2c08cdefbe06b4496244d4f6
daehan.lawplatform <daehan@lawplatform.kr>
2023-08-29 12:17:40 +0900

    feat: add meta html for navea search

---
+++ app/naver85e1d75b33523acde65f48ba4a1b1a7e/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "@/src/components/Hero";
+import Slide from "@/src/components/Slide";
+
+export default function Home() {
+	return (
+		<div className=" bg-black ">
+			naver-site-verification: naver85e1d75b33523acde65f48ba4a1b1a7e.html
+		</div>
+	);
+}
